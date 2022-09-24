6adc9b7c5560fffaa6e64d862c15e1e7acd58d2e
Tanvir Hassan <mthnies@gmail.com>
2022-09-24 02:11:30 -0400

    added variable declaration way

---
+++ Tutorial 3/Variable_Declaration/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    //way to declare integar variable (non-decimal numbers) separately
+    int number1;
+    int number2;
+
+    //another way to declare integar variable together
+    int number3, number4;
+
+    //way to declare float variable separately
+    int float1;
+    int float2;
+
+    //another way to declare float variable together
+    int float3, float4;
+
+    //way to declare double variable separately
+    int double1;
+    int double2;
+
+    //another way to declare double variable together
+    int double3, double4;
+
+
+    //way to declare char variable  separately
+    char character1;
+    char character2;
+
+    //another way to declare char variable together
+    char character3, character4;
+
+    return 0;
+}
