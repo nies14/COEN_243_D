c69fbc0c4e58365d285e6c6b9b566f66efe11312
Tanvir Hassan <mthnies@gmail.com>
2022-10-19 01:11:19 -0400

    fixed initilaization

--- Tutorial 6/Exercise5.cpp
+++ Tutorial 6/Exercise5.cpp
@@ -11,7 +11,7 @@ int main()
    cout << "Fahrenheit equivalents of Celsius temperatures:" << endl;
 
    // create 4 sets of table headers
-   for (unsigned int t{0}; t < 4; ++t) 
+   for (unsigned int t = 0; t < 4; ++t) 
    {
       cout << setw(7) << "Celsius" << setw(12) << "Fahrenheit ";
    }
@@ -19,9 +19,9 @@ int main()
    cout << endl;
 
    // display temperatures in blocks of 25
-   for (unsigned int i{0}; i < 25; ++i) 
+   for (unsigned int i = 0; i < 25; ++i) 
    {
-      for (unsigned int j{0}; j <= 75; j += 25) 
+      for (unsigned int j = 0; j <= 75; j += 25) 
       {
          cout << setw(7) << i + j << setw(11) << fahrenheit(i + j) << ' ';
       }
@@ -36,16 +36,16 @@ int main()
    cout << "\nCelsius equivalents of Fahrenheit temperatures:" << endl;
 
    // create 4 sets of table headers
-   for (unsigned int t{0}; t < 4; ++t) {
+   for (unsigned int t = 0; t < 4; ++t) {
       cout << setw(10) << "Fahrenheit" << setw(9) << "Celsius ";
    }
 
    cout << endl;
 
    // display temperatures in blocks of 45
-   for (unsigned int i{32}; i < 77; ++i) 
+   for (unsigned int i = 32; i < 77; ++i) 
    {
-      for (unsigned int j{0}; j <= 135; j += 45) 
+      for (unsigned int j = 0; j <= 135; j += 45) 
       {
          cout << setw(10) << i + j << setw(8) << celsius(i + j) << ' ';
       }
